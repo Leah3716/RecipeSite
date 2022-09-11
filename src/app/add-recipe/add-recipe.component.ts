8a9e010488b6a71b96368428d632dd62394fb9c2
Leah3716 <leahe93716@gmail.com>
2022-09-12 01:56:05 +0300

    changes

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import{Recipe}from 'src/app/Recipe';
+import { Recipe } from 'src/app/Recipe';
 import { Router } from '@angular/router';
-import {RecipeService }from '../recipe-service';
+import { RecipeService } from '../recipe-service';
 import swal from 'sweetalert';
 @Component({
   selector: 'app-add-recipe',
@@ -10,44 +10,44 @@ import swal from 'sweetalert';
 })
 export class AddRecipeComponent implements OnInit {
 
-  arr:number[];
-arrRecipe:Recipe[];
-newRecipe:Recipe=new Recipe(null,null,null,null,null,new Date(),[""],[""], JSON.parse(localStorage.getItem("currentUser")).Id,null);
-  constructor(public recSer:RecipeService,public myRouter:Router) {
+  arr: number[];
+  arrRecipe: Recipe[];
+  newRecipe: Recipe = new Recipe(null, null, null, null, null, new Date(), [""], [""], JSON.parse(localStorage.getItem("currentUser")).Id, null);
+  constructor(public recSer: RecipeService, public myRouter: Router) {
     this.recSer.getRecipe().subscribe(
-      succ=>{this.arrRecipe=succ;},
-      err=>{console.log(err);})
-      
-   }
-  change(value,arr,i){
-      if(value!="")
-      {
-       
-        arr[i]=value;
-        arr[i+1]="";
-        arr.splice(i+1,1)   
-      }
-      else{
-        arr.splice(i-1,1)
-      }
-    if(arr.length==0)
-    arr[0]="";
+      succ => { this.arrRecipe = succ; },
+      err => { console.log(err); })
+
+  }
+  change(value, arr, i) {
+    if (value != "") {
+
+      arr[i] = value;
+      arr[i + 1] = "";
+      arr.splice(i + 1, 1)
+    }
+    else {
+      arr.splice(i - 1, 1)
+    }
+    if (arr.length == 0)
+      arr[0] = "";
   }
   add(): void {
-  
-    this.newRecipe.Id= this.arrRecipe.length.toString();
-    this.newRecipe.ImgRouting="assets/img/red.jpg";
-    this.newRecipe.CategoryId="1";
-  this.myRouter.navigate(["reciepe"]);
-  this.recSer.AddRecipe(this.newRecipe).subscribe(
-    succ=>{swal("Good job!", "המתכון נוסף בהצלחה!", "success");
-  },
-    err=>{alert("HUGE!")}
-  )
+
+    this.newRecipe.Id = this.arrRecipe.length.toString();
+    this.newRecipe.ImgRouting = "assets/img/red.jpg";
+    this.newRecipe.CategoryId = "1";
+    this.myRouter.navigate(["reciepe"]);
+    this.recSer.AddRecipe(this.newRecipe).subscribe(
+      succ => {
+        swal("Good job!", "המתכון נוסף בהצלחה!", "success");
+      },
+      err => { alert("HUGE!") }
+    )
   }
- 
+
   ngOnInit(): void {
- 
+
   }
 
 }
